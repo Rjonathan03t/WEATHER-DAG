2568b32d61330a7d79ed180048a495be8cff204f
Artiana <hei.rojo.3@gmail.com>
2024-08-06 22:04:30 +0300

    refactor

--- load.py
+++ load.py
@@ -1,21 +1,2 @@
-import pandas as pd
-import matplotlib.pyplot as plt
-
-# Charger les données
-df = pd.read_csv('air_pollution_data_complete.csv')
-
-# Afficher les premières lignes
-print(df.head())
-
-# Statistiques descriptives
-print(df.describe())
-
-# Visualisation des tendances
-plt.figure(figsize=(10, 6))
-plt.plot(df['Location'], df['AQI'], marker='o')
-plt.title('AQI par Localisation')
-plt.xlabel('Location')
-plt.ylabel('AQI')
-plt.xticks(rotation=45)
-plt.grid(True)
-plt.show()
+def load_data():
+    pass
