b582df05e2fae5b56e0a1477a7bde210c2e55a9f
Artiana <hei.rojo.3@gmail.com>
2024-08-06 20:14:34 +0300

    to extract data to csv file

--- get_air_pollution.py
+++ get_air_pollution.py
@@ -1,13 +1,14 @@
 import requests
 import json
+from save_data_to_csv import save_data_to_csv
 
 def get_air_pollution_data(lat, lon, api_key):
     url = f"http://api.openweathermap.org/data/2.5/air_pollution?lat={lat}&lon={lon}&appid={api_key}"
-    headers = {'User-Agent': 'My Python App'}  # Ajoutez un User-Agent
+    headers = {'User-Agent': 'My Python App'}
 
     try:
         response = requests.get(url, headers=headers)
-        response.raise_for_status()  # Raise an exception for error HTTP status codes
+        response.raise_for_status()
         data = response.json()
         return data
     except requests.exceptions.RequestException as e:
@@ -21,9 +22,10 @@ def print_data(data):
 # Exemple pour Paris
 latitude = 48.8566  
 longitude = 2.3522  
-
-
 api_key = '4cbf2ec40fa4073d64fa831d6efcef60'
 
 data = get_air_pollution_data(latitude, longitude, api_key)
 print_data(data)
+
+if data:
+    save_data_to_csv(data, 'air_pollution_data.csv')
