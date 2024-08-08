73b364965a3f98580695e0e8b8f092aabb8028cb
Artiana <hei.rojo.3@gmail.com>
2024-08-08 09:26:54 +0300

    fix:fix load data

--- script/load.py
+++ script/load.py
@@ -2,4 +2,4 @@ import pandas as pd
 
 def load_data():
     df = pd.read_csv('/tmp/geo_transformed_temp.csv')
-    df.to_csv('geographie_pollution.csv', index=False)
+    df.to_csv('/tmp/geographie_pollution.csv', index=False)
